bc854b316c13488e68e52080e4f1ad02a635e3ce
Mehdi <borlandology@gmail.com>
2026-05-31 15:58:40 +0330

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,31 @@
+export default async function handler(req, res) {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+  
+  try {
+    const { telegram_link } = req.body;
+    
+    const response = await fetch('https://telegramdownloader.net/proxy.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({ telegram_link })
+    });
+    
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
